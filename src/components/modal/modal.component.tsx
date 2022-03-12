91a3c7e1910272be542df16c57d773185fa42cb6
slavik <fedorik83@gmail.com>
2022-03-13 01:47:09 +0300

    created useDisableByScroll hook

--- src/components/modal/modal.component.tsx
+++ src/components/modal/modal.component.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useRef } from "react";
+import useDisableByScroll from "../../hooks/useDisableByScroll";
 import { ModalLayout, TeaPhoto, CloseModalButton } from "./modal.styles";
 
 interface IModalProps {
@@ -8,15 +9,18 @@ interface IModalProps {
 
 const Modal: FC<IModalProps> = ({ selectedImage, setSelectedImage }) => {
   const modalRef = useRef<HTMLDivElement>(null);
+  const isOpen: boolean = selectedImage ? true : false;
 
-  const handleClick = (e: React.MouseEvent) => {
+  useDisableByScroll(isOpen);
+
+  const handleOuterClose = (e: React.MouseEvent) => {
     if (modalRef.current === e.target) {
       setSelectedImage(null);
     }
   };
 
   return (
-    <ModalLayout ref={modalRef} onClick={handleClick}>
+    <ModalLayout ref={modalRef} onClick={handleOuterClose}>
       <TeaPhoto src={selectedImage} alt="" />
       <CloseModalButton onClick={() => setSelectedImage(null)} />
     </ModalLayout>
---
+++ src/hooks/useDisableByScroll.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+
+const useDisableByScroll = (isOpen: boolean) => {
+  useEffect(() => {
+    if (isOpen) document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  });
+};
+
+export default useDisableByScroll;
